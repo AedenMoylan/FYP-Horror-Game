71d0f43709254402514a9ce8cd8548f2dc2c9716
AedenMoylan <58327062+AedenMoylan@users.noreply.github.com>
2023-01-22 16:39:04 +0000

    added animator with moving killer model

---
+++ FYP-Horror/Assets/Scripts/KillerScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Progress;
+
+public class KillerScript : MonoBehaviour
+{
+    private Animator m_Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        handleInput();
+
+        if (m_Animator)
+        {
+            Vector3 newPosition = transform.position;
+            newPosition.z += m_Animator.GetFloat("Runspeed") * Time.deltaTime;
+            transform.position = newPosition;
+        }
+    }
+
+    private void handleInput()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            print("1 key was pressed");
+            m_Animator.SetBool("Alert", true);
+            m_Animator.SetBool("Walking", false);
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            print("2 key was pressed");
+            m_Animator.SetBool("Walking", true);
+            m_Animator.SetBool("Alert", false);
+        }
+    }
+}
